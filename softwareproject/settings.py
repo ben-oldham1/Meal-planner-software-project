fe19f2612dc5b26dac2878196e9bca353ac57f0e
Ben <ben.oldham2003@gmail.com>
2024-10-20 15:35:40 +0100

    Setup authentication

--- softwareproject/settings.py
+++ softwareproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# General config
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
