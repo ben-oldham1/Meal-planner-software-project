7c5da1b88f9a9c05f5ba93fb789cc29421ab73eb
Ben <ben.oldham2003@gmail.com>
2024-10-20 16:58:14 +0100

    Edit weekdays

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -10,19 +10,19 @@ DIFFICULTY_CHOICES = [
 ]
 
 WEEKDAY_CHOICES = [
-    (0, 'Sunday'),
-    (1, 'Monday'),
-    (2, 'Tuesday'),
-    (3, 'Wednesday'),
-    (4, 'Thursday'),
-    (5, 'Friday'),
-    (6, 'Saturday'),
+    (0, 'Monday'),
+    (1, 'Tuesday'),
+    (2, 'Wednesday'),
+    (3, 'Thursday'),
+    (4, 'Friday'),
+    (5, 'Saturday'),
+    (6, 'Sunday'),
 ]
 
 MEASUREMENT_UNITS = [
     ('g', 'grams'),
     ('ml', 'milliliters'),
-    ('unit', 'unit(s)'),
+    ('unit', 'units'),
 ]
 
 class Recipe(models.Model):
