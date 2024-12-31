bee2ff9aa1a5287b337e3f6d3dd65147c888d580
Ben <ben.oldham2003@gmail.com>
2024-12-31 16:33:53 +0000

    Add django filters package

--- softwareproject/settings.py
+++ softwareproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'accounts',
     'crispy_forms',
     'crispy_bootstrap5',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
